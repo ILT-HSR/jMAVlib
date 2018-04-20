b4538478b312d0ef0f3e3dab9423ec821e00ccfb
Felix Morgner <fmorgner@hsr.ch>
2018-04-20 12:03:52 +0200

    tests: fix unit tests

--- src/test/java/me/drton/jmavlib/mavlink/MAVLinkMessageTest.kt
+++ src/test/java/me/drton/jmavlib/mavlink/MAVLinkMessageTest.kt
@@ -1,6 +1,6 @@
 package me.drton.jmavlib.mavlink
 
-import me.drton.jmavlib.createMAVLinkHeartbeat
+import me.drton.jmavlib.createHeartbeatMessage
 import me.drton.jmavlib.support.ByteStringChannel
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert.assertThat
@@ -13,17 +13,20 @@ import org.robolectric.RuntimeEnvironment
 @RunWith(RobolectricTestRunner::class)
 class MAVLinkMessageTest {
 
+    private lateinit var schema: MAVLinkSchema
+
     @Before
     fun setUp() {
-        MAVLinkSchemaRegistry.instance[RuntimeEnvironment.application, "common"] = "schemas/common.xml"
+        MAVLinkSchemaRegistry[RuntimeEnvironment.application, "common"] = "schemas/common.xml"
+        schema = MAVLinkSchemaRegistry["common"]!!
     }
 
     @Test
     fun heartbeat_messageIsSerializedCorrectly() {
-        val heartbeat = createMAVLinkHeartbeat(system = 8, component = 250)
+        val heartbeat = createHeartbeatMessage(8, 250, schema)
         val channel = ByteStringChannel()
 
-        MAVLinkStream(MAVLinkSchemaRegistry.instance["common"], channel).write(heartbeat)
+        MAVLinkStream(schema, channel).write(heartbeat)
 
         assertThat(channel.toString(), equalTo("0xfe\n0x09\n0x00\n0x08\n0xfa\n0x00\n0x00\n0x00\n0x00\n0x00\n0x06\n0x00\n0x80\n0x04\n0x00\n0xb9\n0x7c"))
     }
