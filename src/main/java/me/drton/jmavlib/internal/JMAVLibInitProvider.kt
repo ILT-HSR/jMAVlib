0bf746ae5ad572ffaef060d16bbc3bb595c356fa
Felix Morgner <fmorgner@hsr.ch>
2019-10-30 15:32:39 +0100

    build: remove build configuration

--- src/main/java/me/drton/jmavlib/internal/JMAVLibInitProvider.kt
+++ src/main/java/me/drton/jmavlib/internal/JMAVLibInitProvider.kt
@@ -12,29 +12,29 @@ private const val SCHEMAS = "schemas"
 
 class JMAVLibInitProvider : ContentProvider() {
 
-    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? = null
-
     override fun onCreate(): Boolean {
-
-        context.assets.list(SCHEMAS).forEach {
-            val filenameParts = it.split(".")
-            when (filenameParts.size) {
-                2 -> {
-                    MAVLinkSchemaRegistry[context, filenameParts[0]] = "$SCHEMAS/$it"
+        context?.run {
+            assets.list(SCHEMAS)?.forEach {
+                val filenameParts = it.split(".")
+                when (filenameParts.size) {
+                    2 -> {
+                        MAVLinkSchemaRegistry[this, filenameParts[0]] = "$SCHEMAS/$it"
+                    }
+                    else -> Log.e(TAG, "Skipping invalid schema file '$SCHEMAS/$it'")
                 }
-                else -> Log.e(TAG, "Skipping invalid schema file '$SCHEMAS/$it'")
             }
         }
-
         return true
     }
 
-    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int = 0
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? = null
 
-    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?): Int = 0
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int = 0
 
-    override fun getType(uri: Uri?): String? = null
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
 
-    override fun insert(uri: Uri?, values: ContentValues?): Uri? = null
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
 
 }
